fbe36aaf074ffa7139dc164506ed710327934219
Ali Amir <muhammadaliamir24@gmail.com>
2025-08-31 06:53:53 +0500

    default route.

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.use(json());
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the Token-Based-Auth API' }));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
